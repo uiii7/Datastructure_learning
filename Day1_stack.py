78effade51f7ad3e580f3c858b1f945ac6bc24c6
uiiii7 <486097781@qq.com>
2026-01-21 20:36:29 +0800

    Define a stack using list in Python

---
+++ Day1_stack.py
@@ -0,0 +1,32 @@
+#Define a stack using list
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def printStack(self):
+        for item in self.items:
+            print(item)
+
+#main
+if __name__ == '__main__':
+    s = Stack()
+    s.push(4)
+    s.push("aaa")
+    s.push(True)
+    s.printStack()
