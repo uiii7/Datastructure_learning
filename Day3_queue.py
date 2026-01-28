df092e7c900eb9d10db36d550bde70f94e79e96c
uiiii7 <486097781@qq.com>
2026-01-28 19:14:40 +0800

    Queue

---
+++ Day3_queue.py
@@ -0,0 +1,29 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, item):
+        self.queue.insert(0, item)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        return self.queue.pop()
+
+    def size(self):
+        return len(self.queue) if not self.isEmpty() else None
+
+    def printQueue(self):
+        print(self.queue)
+
+
+q = Queue()
+for i in range(10):
+    q.enqueue(i)
+
+q.printQueue()
+q.dequeue()
+q.printQueue()
